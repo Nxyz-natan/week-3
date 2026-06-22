777d02b0839b86744065f3adf0f83c5afa258625
Nxyz-natan <natanyemane75@icloud.com>
2026-06-21 21:15:56 -0600

    load task and saving taks

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import os
+import os 
 import json
+from typing import Optional
 
 app = FastAPI()
 
-TASKS_FILE = "tasks.json"
+TASK_FILE = "tasks.json"
 
 class TaskBody(BaseModel):
     task: str
+    priority: str = "medium"
 
 def load_tasks():
-    if not os.path.exists(TASKS_FILE):
+    if not os.path.exists(TASK_FILE):
         return []
-    with open(TASKS_FILE, "r") as file:
+    with open(TASK_FILE, "r") as file:
         return json.load(file)
 
 def save_tasks(tasks):
-    with open(TASKS_FILE, "w") as file:
+    with open(TASK_FILE, "w") as file:
         json.dump(tasks, file, indent=2)
-    
+
 @app.get("/tasks")
-async def get_tasks():
-    return load_tasks()
+async def get_tasks(done: Optional[bool] = None):
+    tasks = load_tasks()
+    if done is not None:
+        tasks = [t for t in tasks if t["done"] == done]
+    return tasks
+
